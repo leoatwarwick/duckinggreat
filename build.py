cd58381fe5b5f31f2dae0ef267cdde87359eb69f
Leo Riviera <leo.riviera@voxa.dev>
2021-03-14 04:35:35 +0000

    Add images in reverse-chronological order

--- build.py
+++ build.py
@@ -1,16 +1,23 @@
+from os.path import getmtime
 import chevron
 import os
 
+os.chdir("./img")
+
+files = reversed(
+    sorted(filter(os.path.isfile, os.listdir('.')), key=os.path.getmtime))
+
 images = []
 
-for item in os.listdir("./img"):
-    if os.path.isfile(f'./img/{item}') and (item.endswith((".png", ".jpg", ".jpeg"))):
+
+for item in files:
+    if os.path.isfile(item) and (item.endswith((".png", ".jpg", ".jpeg"))):
         images.append(item)
 
-with open("./index.mustache", "r") as template:
+with open("../index.mustache", "r") as template:
     index_html = chevron.render(template, {
         "images": images
     })
 
-    with open("./index.html", "w") as index:
+    with open("../index.html", "w") as index:
         index.write(index_html)
