285dfc63b160aa8bd17ec563e788e26b07f057d8
Leo Riviera <leo.riviera@voxa.dev>
2021-03-14 13:52:32 +0000

    Add compression to build script

--- build.py
+++ build.py
@@ -1,8 +1,9 @@
 from os.path import getmtime
+from PIL import Image
 import chevron
 import os
 
-os.chdir("./img")
+os.chdir("./images")
 
 files = reversed(
     sorted(filter(os.path.isfile, os.listdir('.')), key=os.path.getmtime))
@@ -11,7 +12,18 @@ images = []
 
 for item in files:
     if os.path.isfile(item) and (item.endswith((".png", ".jpg", ".jpeg"))):
-        images.append(f"./img/{item}")
+        path = f"./images/{item}"
+        images.append(path)
+
+        duck_photo = Image.open(item)
+
+        base_width = 768
+        width_percentage = base_width / float(duck_photo.size[0])
+        height = int(float(duck_photo.size[1]) * float(width_percentage))
+
+        duck_photo = duck_photo.resize((base_width, height), Image.LANCZOS)
+
+        duck_photo.save(item, optimize=True, quality=75)
 
 with open("../index.mustache", "r") as template:
     index_html = chevron.render(template, {
